eb58de0db65bc71b28d33ea53e86977839a11134
Armin Sander <armin@replicator.org>
2021-01-22 18:37:04 +0100

    rust-skia: Support Catalina based builds

--- src/gpu/mtl/GrMtlPipelineStateBuilder.mm
+++ src/gpu/mtl/GrMtlPipelineStateBuilder.mm
@@ -672,7 +672,12 @@ GrMtlPipelineState* GrMtlPipelineStateBuilder::finalize(
 
     NSError* error = nil;
 #if GR_METAL_SDK_VERSION >= 230
-    if (@available(macOS 11.0, iOS 14.0, *)) {
+
+    // rust-skia: As long our build platforms are on macOS 10, we can't support
+    // macOS 11 @available checks yet, which fail with a
+    // __isPlatformVersionAtLeast linker error.
+
+    if (false /* @available(macOS 11.0, iOS 14.0, *) */) {
         id<MTLBinaryArchive> archive = fGpu->binaryArchive();
         if (archive) {
             NSArray* archiveArray = [NSArray arrayWithObjects:archive, nil];
@@ -805,7 +810,10 @@ bool GrMtlPipelineStateBuilder::PrecompileShaders(GrMtlGpu* gpu, const SkData& c
 
     NSError* error = nil;
 #if GR_METAL_SDK_VERSION >= 230
-    if (@available(macOS 11.0, iOS 14.0, *)) {
+    // rust-skia: As long our build platforms are on macOS 10, we can't support
+    // macOS 11 @available checks yet, which fail with a
+    // __isPlatformVersionAtLeast linker error.
+    if (false /* @available(macOS 11.0, iOS 14.0, *) */) {
         id<MTLBinaryArchive> archive = gpu->binaryArchive();
         if (archive) {
             NSArray* archiveArray = [NSArray arrayWithObjects:archive, nil];
